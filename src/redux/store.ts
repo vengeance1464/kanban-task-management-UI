87f197fbaba269fcc4c85ac4a4eefaccdd555d95
Priyam Chhabra <priyam.chhabra704@gmail.com>
2023-10-30 23:54:43 +0530

    adding support for local storage

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -9,7 +9,32 @@ const store = configureStore({
     tasks:taskReducer,
     columns:columnsReducer
   },
+  
 });
+
+store.subscribe(() => {
+  // persist your state
+  console.log("state ",store.getState())
+
+  const {board,columns,tasks}=store.getState()
+
+  // const boards =
+  // const tasks=localStorage.getItem('tasks')
+  // const columns=localStorage.getItem('columns')
+
+  // localStorage.setItem('board',boards)
+
+  // localStorage.setItem('tasks',tasks)
+
+  // if(columns!==null)
+  localStorage.setItem('columns',JSON.stringify(columns))
+
+  localStorage.setItem('board',JSON.stringify(board))
+
+  localStorage.setItem('tasks',JSON.stringify(tasks))
+
+
+})
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
